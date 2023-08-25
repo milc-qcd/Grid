5793679b66c08456355c8ccb348b8054d599d267
Carleton DeTar <detar@physics.utah.edu>
2023-08-25 11:47:41 -0400

    For basis rotation, use GPU offload for HIP as well as CUDA

--- Grid/lattice/Lattice_basis.h
+++ Grid/lattice/Lattice_basis.h
@@ -62,7 +62,7 @@ void basisRotate(VField &basis,Matrix& Qt,int j0, int j1, int k0,int k1,int Nm)
     basis_v.push_back(basis[k].View(AcceleratorWrite));
   }
 
-#if ( (!defined(GRID_CUDA)) )
+#if ( (!defined(GRID_CUDA) && !defined(GRID_HIP) ) )
   int max_threads = thread_max();
   Vector < vobj > Bt(Nm * max_threads);
   thread_region
@@ -83,6 +83,8 @@ void basisRotate(VField &basis,Matrix& Qt,int j0, int j1, int k0,int k1,int Nm)
     }
 #else
   View *basis_vp = &basis_v[0];
+
+  std::cout << GridLogMessage << "Using GPU for basis rotation" << std::endl;
 
   int nrot = j1-j0;
   if (!nrot) // edge case not handled gracefully by Cuda
