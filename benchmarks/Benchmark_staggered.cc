be342aca8795e7298658d3b00d6599412daa5f27
Carleton DeTar <detar@physics.utah.edu>
2023-08-31 15:52:11 -0400

    Always instantiate double-precision fermions

--- benchmarks/Benchmark_staggered.cc
+++ benchmarks/Benchmark_staggered.cc
@@ -93,7 +93,7 @@ int main (int argc, char ** argv)
   RealD c1=9.0/8.0;
   RealD c2=-1.0/24.0;
   RealD u0=1.0;
-  ImprovedStaggeredFermionR Ds(Umu,Umu,Grid,RBGrid,mass,c1,c2,u0,params);
+  ImprovedStaggeredFermionD Ds(Umu,Umu,Grid,RBGrid,mass,c1,c2,u0,params);
   Ds.ZeroCounters();
   
   std::cout<<GridLogMessage << "Calling Ds"<<std::endl;
